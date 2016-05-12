8331ac99ec64e57b793d8e4f2f8d35f080ac6793
Omri Kaplan <omrikap@gmail.com>
2016-05-12 11:59:06 +0300

    Makefile, CMake, and a basic test script working

---
+++ Debug/basic_test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#A simple test script for the CachingFileSystem
+
+#define the path for the mount and root dir
+BIN_PATH=/media/sf_huji/courses/os/hw/ex4/Debug
+ROOTDIR=$BIN_PATH/root_dir
+MOUNTDIR=$BIN_PATH/mount_dir
+
+# unmount the last mount
+fusermount -u -q /media/sf_huji/courses/os/hw/ex4/Debug/mount_dir
+
+$BIN_PATH/CachingFileSystem $ROOTDIR $MOUNTDIR
+
+# make sure nothing stays mounted
+fusermount -u /media/sf_huji/courses/os/hw/ex4/Debug/mount_dir
+echo "Unmounted FUSE"
+
+echo "Done"
