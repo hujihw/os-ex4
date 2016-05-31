d5d372ef7830d55f0734cf116d550e52f2f23870
Omri Kaplan <omrikap@gmail.com>
2016-06-01 00:51:36 +0300

    frame function declarations for cache system

--- CacheManager.cpp
+++ CacheManager.cpp
@@ -4,5 +4,3 @@
 
 // todo create a data structure for every part of the cache
 
-
-
--- CacheManager.h
+++ CacheManager.h
@@ -4,20 +4,48 @@
 #define EX4_CACHEMANAGER_H
 
 #include <unordered_map>
-#include <map>
+#include <list>
 #include <string>
 #include <functional>
 #include "CacheBlock.h"
 
-typedef std::unordered_map<std::hash<char*>, CacheBlock> blocks_map;
-typedef std::map<std::string, blocks_map> cache_chain;
-
-
+typedef std::unordered_map<std::hash<char*>, CacheBlock> blocks_map; // todo what is the exact type of the buffers used as keys?
+typedef std::list<CacheBlock> cache_chain;
 
 class CacheManager {
-
-
     cache_chain chain;
+
+    // functions to implement
+
+    /**
+     * @brief Find the given block in cache
+     */
+    CacheBlock *findBlock(); // todo should be (void *)?
+
+    /**
+     * @brief Retrieve a block from the cache (get it's value), should use findBlock.
+     */
+    CacheBlock *retrieveBlock();
+
+    /**
+     * @brief Insert a new block to the cache. Should use moveToHead().
+     */
+    void insertBlock();
+
+    /**
+     * @brief Remove a block from the cache. Should use removeTail().
+     */
+    void removeBlock();
+
+    /**
+     * @brief Move a block to the head of the list, and change relevant pointers.
+     */
+    void moveToHead();
+
+    /**
+     * @brief Remove the block at the tail of the list, and change relevant pointers.
+     */
+    void removeTail();
 };
 
 
