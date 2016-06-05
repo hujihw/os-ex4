3e5be98d698bede304fd8cbaaf5f2e8134cd286d
Omri Kaplan <omrikap@gmail.com>
2016-06-05 19:21:33 +0300

    added fflush to log operations

--- logManager.cpp
+++ logManager.cpp
@@ -19,8 +19,10 @@ void log_call(const char *callName) {
     time(&callTime);
 
     fprintf((CF_LOG->logfile), "%li %s\n", callTime, callName);
+    fflush(CF_LOG->logfile);
 }
 
 void ioctl_log(const char *cached_files) {
-    fprintf((CF_LOG->logfile), "%s\n", cached_files);
+    fprintf((CF_LOG->logfile), "%s", cached_files);
+    fflush(CF_LOG->logfile);
 }
